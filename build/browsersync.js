23977d293272616f346990a7700f241500ba87d9
Brittany Chiang <brittany_chiang@apple.com>
2017-10-05 17:01:38 -0700

    add images to watch

--- build/browsersync.js
+++ build/browsersync.js
@@ -3,6 +3,7 @@ const browserSync  = require('browser-sync').create();
 const htmlPath     = 'src/*.html';
 const scssPath     = 'src/scss/**/*.scss';
 const jsPath       = 'src/js/*.js';
+const imgPath      = 'src/img/**/*.+(png|jpg|gif|svg)';
 
 module.exports = gulp => {
   gulp.task('serve', () => {
@@ -13,8 +14,9 @@ module.exports = gulp => {
       }
     });
 
+    gulp.watch(htmlPath, ['html', browserSync.reload]);
     gulp.watch(scssPath, ['sass', browserSync.reload]);
     gulp.watch(jsPath, ['scripts', browserSync.reload]);
-    gulp.watch(htmlPath, ['html', browserSync.reload]);
+    gulp.watch(imgPath, ['images', browserSync.reload]);
   });
 }
